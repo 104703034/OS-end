52b0830b0b643748b67a7f84cd34d9aa3e1b9c7a
Lohan <Lohan@github.com>
2018-01-12 04:03:14 +0800

    unity try

---
+++ unity/Assets/control.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class control : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnTriggerEnter(Collider col){
+		if (col.CompareTag ("wall") == true) {
+			isok = false;
+			Destroy (this.gameObject);
+		}
+	}
+}
